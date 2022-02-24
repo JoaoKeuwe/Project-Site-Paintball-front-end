40e35fcc60b1609dfceb817ad184cf3559c74e77
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-02-24 09:21:16 -0300

    adicionando localStorage, e validação no email

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,30 +3,88 @@ import logo from '../image/logo.png';
 import '../styles/Login.css'
 
 
+
 class Login extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: '',
+            isValid: false
+        }
+    }
 
     handleClick = () => {
+        const {email, password} = this.state;
         const {history} = this.props;
-        history.push('/form')
+
+        if (this.validEmail(email)) this.setState ({isValid: true}, () => {
+            const {isValid} = this.state;
+            if (isValid) history.push('/form')
+        })
+
+       
+
+        localStorage.setItem("infoUser", JSON.stringify({email, password}));
+    
     }
+
+    takeInfoUser = (event) => {
+        const {value, name} = event.target
+        this.setState ({
+            [name]:value
+        })
+    }
+
+        validEmail = (email) => {
+            const emailRegex = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
+        return emailRegex.test(email)
+        
+
+    }
+    
+
+
     render() { 
         return (
             <section className="login">
+
                 <article >
+
                     <img 
                     src={logo} alt="logo imagem"
                     className="image-login" />
+
                 </article>
-                <form>
-                    <label className="label-email" htmlFor="input-email" className="input-email">
-                        
-                    <input type="text" placeholder="Email" name="input-email" id="input-email" className="input-email" />
+
+                <form className="label-email" >
+
+                    <label 
+                    htmlFor="email" className="input-email">
+                      
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    id="email"
+                    
+                    onChange={this.takeInfoUser}
+                    className="input-email"
+                    />
+                   
+
                     </label>
 
                     <label htmlFor="input-password"
                     className="input-password">
-                      
-                    <input type="password" placeholder="Enter your password"className="input-password" name="input-passwordl" id="input-password" />
+                     
+                    <input 
+                    type="password" 
+                    placeholder="Enter your password"
+                    className="input-password" 
+                    name="password"
+                    onChange={this.takeInfoUser}
+                    id="password" />
                     </label>
 
                     <button
