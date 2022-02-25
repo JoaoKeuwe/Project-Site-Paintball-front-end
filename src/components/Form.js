c35068504c71c519e806e63273fb31eed19c88b8
marinafischer <marinamirandafischer@gmail.com>
2022-02-25 16:44:59 -0300

    style: :lipstick: form style

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,8 @@
-import { hasSelectionSupport } from '@testing-library/user-event/dist/utils';
 import React, { useState } from 'react';
+import '../styles/Form.css';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 const initiaFormState = {
   playerName: '',
@@ -29,169 +32,305 @@ function Form () {
     setForm(form => ({...form, ...value}))
   }
 
+  const handleSubmit = () => {
+    setForm(initiaFormState);
+  }
+
   const firstForm = (
-    <form id="firsForm">
-      <label htmlFor="inputName"> Nome:
-        <input 
-          id="inputName"
-          type="tex"
-          placeholder="Humberto Silva"
-          onChange={(e) => handleInputChange({playerName: e.target.value})}
-        />
-      </label>
-      <label htmlFor="inputAge"> Idade:
-        <input
-          id="inputAge"
-          type="number"
-          onChange={(e) => handleInputChange({age: e.target.value})} 
-        />
-      </label>
-      <label htmlFor="inputDescription"> Descrição
-        <textarea
-          id="inputDescription"
-          onChange={(e) => handleInputChange({description: e.target.value})}
-        />
-      </label>
-      <label htmlFor="inputPicture"> Foto:
-        <input id="inputPicture" type="file" />
-      </label>
-      <div>
-        <span>Características:</span>
-        <label htmlFor="inputStrength"> Força:
-          <input
-            id="inputStrength"
-            type="range"
-            min="1"
-            max="10"
-            onChange={(e) => handleInputChange({str: e.target.value})}
-          />
-        </label>
-        <label htmlFor="inputEquipment"> Equipamento:
-          <input
-            id="inputEquipment"
-            type="range"
-            min="1"
-            max="10"
-            onChange={(e) => handleInputChange({eqp: e.target.value})}
-          />
-        </label>
-        <label htmlFor="inputMovement"> Movimentação:
-          <input
-            id="inputMovement"
-            type="range"
-            min="1"
-            max="10"
-            onChange={(e) => handleInputChange({mov: e.target.value})}
-          />
-        </label>
-        <label htmlFor="inputInteligence"> Inteligência:
-          <input
-            id="inputInteligence"
-            type="range"
-            min="1"
-            max="10"
-            onChange={(e) => handleInputChange({int: e.target.value})}
-          />
-        </label>
-        <label htmlFor="inputAim"> Mira:
-          <input
-            id="inputAim"
-            type="range" 
-            min="1" 
-            max="10"
-            onChange={(e) => handleInputChange({aim: e.target.value})}
-          />
-        </label>
-      </div>
-      <button
-        type="button"
-        onClick={()=>{setView(2)}}
-      >Próximo</button>
-    </form>
+    <Container>
+      <form id="firsForm">
+        <Row>
+          <Col sm={8}>
+            <label htmlFor="inputName"> Nome:
+              <br />
+              <input 
+                className = "InputForm"
+                value={form.playerName} 
+                id="inputName"
+                type="tex"
+                placeholder="Humberto Silva"
+                onChange={(e) => handleInputChange({playerName: e.target.value})}
+              />
+            </label>
+          </Col>
+          <Col sm={4}>
+            <label htmlFor="inputAge"> Idade:
+              <br />
+              <input
+                className = "InputForm"
+                value={form.age}
+                id="inputAge"
+                type="number"
+                onChange={(e) => handleInputChange({age: e.target.value})} 
+              />
+            </label>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label htmlFor="inputDescription"> Descrição:
+              <br />
+              <textarea
+                className = "InputForm"
+                value={form.description}
+                id="inputDescription"
+                onChange={(e) => handleInputChange({description: e.target.value})}
+              />
+            </label>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label htmlFor="inputPicture"> Foto:
+              <br />
+              <input
+                className = "InputForm"
+                value={form.picture}
+                id="inputPicture"
+                type="text"
+                onChange={(e) => handleInputChange({picture: e.target.value})} 
+              />
+            </label>
+          </Col>
+        </Row>
+        <div className="characteristicsContainer">
+          <span>Características:</span>
+          <Row>
+            <Col>
+              <label htmlFor="inputStrength"> Força:
+                <br />
+                <input
+                  className="RangeInput"
+                  value={form.str}
+                  id="inputStrength"
+                  type="range"
+                  min="1"
+                  max="10"
+                  onChange={(e) => handleInputChange({str: e.target.value})}
+                />
+              </label>
+            </Col>
+            <Col>
+              <label htmlFor="inputEquipment"> Equipamento:
+                <br />
+                <input
+                  className="RangeInput"
+                  value={form.eqp}
+                  id="inputEquipment"
+                  type="range"
+                  min="1"
+                  max="10"
+                  onChange={(e) => handleInputChange({eqp: e.target.value})}
+                />
+              </label>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <label htmlFor="inputMovement"> Movimentação:
+                <br />
+                <input
+                  className="RangeInput"
+                  value={form.mov}
+                  id="inputMovement"
+                  type="range"
+                  min="1"
+                  max="10"
+                  onChange={(e) => handleInputChange({mov: e.target.value})}
+                />
+              </label>
+            </Col>
+            <Col>
+              <label htmlFor="inputInteligence"> Inteligência:
+                <br />
+                <input
+                  className="RangeInput"
+                  value={form.int}
+                  id="inputInteligence"
+                  type="range"
+                  min="1"
+                  max="10"
+                  onChange={(e) => handleInputChange({int: e.target.value})}
+                />
+            </label>
+            </Col>
+          </Row>
+          <Row xs={2}>
+            <Col>
+              <label htmlFor="inputAim"> Mira:
+                <br />
+                <input
+                  className="RangeInput"
+                  value={form.aim}
+                  id="inputAim"
+                  type="range" 
+                  min="1" 
+                  max="10"
+                  onChange={(e) => handleInputChange({aim: e.target.value})}
+                />
+              </label>
+            </Col>
+          </Row>
+        </div>
+        <button
+          className="BlueButton"
+          type="button"
+          onClick={()=>{setView(2)}}
+        >Próximo</button>
+      </form>
+    </Container>
   )
 
   const secondForm = (
-    <form id="secondForm">
-      <label htmlFor="inputTeam"> Time:
-        <input 
-          id="inputTeam"
-          type="tex"
-          placeholder="Sombra Jade"
-          onChange={(e) => handleInputChange({team: e.target.value})}
-        />
-      </label>
-      <label htmlFor="inputRole"> Função:
-        <select
-          id="inputRole"
-          onChange={(e) => handleInputChange({role: e.target.value})}
-        >
-          <option value="burro">Burro</option>
-          <option value="capitão">Capitão</option>
-          <option value="sniper">Sniper</option>
-          <option value="lurker">Lurker</option>
-          <option value="coringa">Coringa</option>
-        </select>
-      </label>
-      <label htmlFor="inputPriWeapon"> Armamento Primário:
-        <select
-          id="inputPriWeapon"
-          onChange={(e) => handleInputChange({priWeapon: e.target.value})}
-          >
-          <option value="pistol">pistol</option>
-          <option value="smg">smg</option>
-          <option value="assault rifle">assault rifle</option>
-          <option value="sniper rifle">sniper rifle</option>
-        </select>
-      </label>
-      <label htmlFor="inputSecWeapon"> Armamento Secundário:
-        <select 
-          id="inputSecWeapon"
-          onChange={(e) => handleInputChange({secWeapon: e.target.value})}
-        >
-          <option value="pistol">pistol</option>
-          <option value="smg">smg</option>
-          <option value="assault rifle">assault rifle</option>
-          <option value="sniper rifle">sniper rifle</option>
-        </select>
-      </label>
-      <div>
-        Partidas:
-        <input
-          type="number"
-          placeholder="Amistosos"
-          onChange={(e) => handleInputChange({friendly: e.target.value})}
-        />
-        <input
-          type="number"
-          placeholder="Regional"
-          onChange={(e) => handleInputChange({regional: e.target.value})}
-        />
-        <input
-          type="number"
-          placeholder="Estadual"
-          onChange={(e) => handleInputChange({state: e.target.value})}
-        />
-        <input
-          type="number"
-          placeholder="Nacional"
-          onChange={(e) => handleInputChange({national: e.target.value})}
-        />
-        <input
-          type="number"
-          placeholder="Internacional"
-          onChange={(e) => handleInputChange({international: e.target.value})}
-        />
-      </div>
-      <button
-        type="button"
-        onClick={()=>{setView(1)}}
-      >Voltar</button>
-      <button type="button">Finalizar</button>
-    </form>
+    <Container>
+      <form id="secondForm">
+        <Row>
+          <Col>
+            <label htmlFor="inputTeam"> Time:
+              <br />
+              <input
+                className = "InputForm"
+                value={form.team} 
+                id="inputTeam"
+                type="tex"
+                placeholder="Sombra Jade"
+                onChange={(e) => handleInputChange({team: e.target.value})}
+              />
+            </label>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label htmlFor="inputRole"> Função:
+              <br />
+              <select
+                className = "InputForm"
+                value={form.role}
+                id="inputRole"
+                onChange={(e) => handleInputChange({role: e.target.value})}
+              >
+                <option value="burro">Burro</option>
+                <option value="capitão">Capitão</option>
+                <option value="sniper">Sniper</option>
+                <option value="lurker">Lurker</option>
+                <option value="coringa">Coringa</option>
+              </select>
+            </label>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label htmlFor="inputPriWeapon"> Armamento Primário:
+              <br />
+              <select
+                className = "InputForm smallInput"
+                value={form.priWeapon}
+                id="inputPriWeapon"
+                onChange={(e) => handleInputChange({priWeapon: e.target.value})}
+                >
+                <option value="pistol">pistol</option>
+                <option value="smg">smg</option>
+                <option value="assault rifle">assault rifle</option>
+                <option value="sniper rifle">sniper rifle</option>
+              </select>
+            </label>
+          </Col>
+          <Col>
+            <label htmlFor="inputSecWeapon"> Armamento Secundário:
+            <br />
+              <select 
+                className = "InputForm smallInput"
+                value={form.secWeapon}
+                id="inputSecWeapon"
+                onChange={(e) => handleInputChange({secWeapon: e.target.value})}
+              >
+                <option value="pistol">pistol</option>
+                <option value="smg">smg</option>
+                <option value="assault rifle">assault rifle</option>
+                <option value="sniper rifle">sniper rifle</option>
+              </select>
+            </label>
+          </Col>
+        </Row>
+        <div>
+          Partidas:
+          <Row>
+            <Col>
+              <input
+                className = "InputForm smallInput"
+                value={form.friendly}
+                type="number"
+                placeholder="Amistosos"
+                onChange={(e) => handleInputChange({friendly: e.target.value})}
+              />
+            </Col>
+            <Col>
+              <input
+                className = "InputForm smallInput"
+                value={form.regional}
+                type="number"
+                placeholder="Regional"
+                onChange={(e) => handleInputChange({regional: e.target.value})}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <input
+                className = "InputForm smallInput"
+                value={form.state}
+                type="number"
+                placeholder="Estadual"
+                onChange={(e) => handleInputChange({state: e.target.value})}
+              />
+            </Col>
+            <Col>
+              <input
+                className = "InputForm smallInput"
+                value={form.national}
+                type="number"
+                placeholder="Nacional"
+                onChange={(e) => handleInputChange({national: e.target.value})}
+              />
+            </Col> 
+          </Row>
+          <Row xs={2}>
+            <Col>
+              <input
+                className = "InputForm smallInput"
+                value={form.international}
+                type="number"
+                placeholder="Internacional"
+                onChange={(e) => handleInputChange({international: e.target.value})}
+              />
+            </Col> 
+          </Row>
+        </div>
+        <Row>
+          <Col>
+            <button
+            className="BlueButton"
+            type="button"
+            onClick={()=>{setView(1)}}
+            >
+              Voltar
+            </button>
+          </Col>
+          <Col>
+            <button 
+              type="button" onClick={handleSubmit}
+              className="GreenButton"
+            >
+              Finalizar
+            </button>
+          </Col> 
+        </Row>
+      </form>
+    </Container>
   )
 
   return (
-    <div>
+    <div className="mainForm">
       {view === 1 && firstForm}
       {view === 2 && secondForm}
     </div>
