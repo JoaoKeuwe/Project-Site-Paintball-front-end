7641284f4f4331ecae86a3a770f8905eab925c13
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-03-03 11:53:47 -0300

    refactor: :monocle_face: formatação no arquivo login.js

--- src/services/Login.js
+++ src/services/Login.js
@@ -1,23 +1,22 @@
-import api from '../services/Api'
+import api from "../services/Api";
 
-const loginPost =  async (username, password) => {
+const loginPost = async (username, password) => {
+  try {
+    const apiPost = await api.post("/login", { username, password });
 
-    try{
-        const apiPost = await api.post('/login', {username, password})
-        
-        const response = {
-          data: apiPost.data,
-          status: apiPost.status
-        }
-        return response
-    } catch ({response}) {
+    const response = {
+      data: apiPost.data,
+      status: apiPost.status,
+    };
+    return response;
+  } catch ({ response }) {
     const result = {
-        data: response.data,
-        status: response.status
-    }
-   
-   return result;
-}
-}
+      data: response.data,
+      status: response.status,
+    };
+
+    return result;
+  }
+};
 
 export default loginPost;
