ed7e4f0a925b3997d621e7c0d1d6e67ce50c8122
marinafischer <marinamirandafischer@gmail.com>
2022-03-03 19:22:17 -0300

    feat: :memo: altera configuração e estilo da table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,31 +1,79 @@
-import React from 'react';
-import logo from '../image/logo.png';
-import Header from '../components/Header';
-import '../styles/Table.css'
+import React from "react";
+import Header from "../components/Header";
+import "../styles/Table.css";
 
-class Table extends React.Component {
-    render() { 
-        return (
-        <div className="table">
-            <Header />
-            <div>   
-                <section >
-                    <input type="text" className="input-header" />
-                </section>
-
-                <section className="informations">
-                    <p>ID</p>
-                    <p>Nome</p>
-                    <p>Função</p>
-                    <p>Time</p>
-                    <p>Armamentos</p>
-                    <p>Características</p>
-                    <p>Partidas</p>
-                </section>
-            </div>
-        </div>
-        );
-    }
+const Table = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <input type="text" className="input-header" />
+        <table className="playersTable">
+          <thead className="tableHeader">
+            <tr className="tableRow">
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Função</th>
+              <th>Time</th>
+              <th>Armamentos</th>
+              <th>Características</th>
+              <th>Partidas</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="playerRow">
+              <td>05</td>
+              <td>Marina</td>
+              <td>Sniper</td>
+              <td>gremio</td>
+              <td>
+                <div>Primário: rifle</div>
+                <div>Secundário: Pistola</div>
+              </td>
+              <td> 
+                <div>força: 8</div>
+                <div>movimentação: 5</div>
+                <div>mira: 3</div>
+                <div>equipamento: 2</div>
+                <div>inteligência: 6</div>
+              </td>
+              <td> 
+                <div>amistoso: 25</div>
+                <div>regional: 13</div>
+                <div>estadual: 5</div>
+                <div>nacional: 2</div>
+                <div>internacional: 0</div>
+              </td>
+            </tr>
+            <tr className="playerRow">
+              <td>05</td>
+              <td>Marina</td>
+              <td>Sniper</td>
+              <td>gremio</td>
+              <td>
+                <div>Primário: rifle</div>
+                <div>Secundário: Pistola</div>
+              </td>
+              <td> 
+                <div>força: 8</div>
+                <div>movimentação: 5</div>
+                <div>mira: 3</div>
+                <div>equipamento: 2</div>
+                <div>inteligência: 6</div>
+              </td>
+              <td> 
+                <div>amistoso: 25</div>
+                <div>regional: 13</div>
+                <div>estadual: 5</div>
+                <div>nacional: 2</div>
+                <div>internacional: 0</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
- 
+
 export default Table;
