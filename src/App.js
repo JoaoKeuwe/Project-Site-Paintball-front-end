a5369ab8dd3c9a5f514969a99b369bf51c3c54ed
Larissa Araujo <larissa.jvsa@gmail.com>
2022-03-05 09:33:44 -0300

    feat: adicionando app no commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import Provider from './context/Provider';
 import Card from "./components/Card";
-// import Radar from "./components/Radar";
-// import RadarNivo from "./components/RadarNivo";
-// import RadarRechart from "./components/RadarRechart";
-// import PieRechart from "./components/PieRechart";
 
 function App() {
   return (
