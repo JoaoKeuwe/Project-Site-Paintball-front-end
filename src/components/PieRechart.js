61c2fa9ab959408acdc59d415bc63102b134934a
Larissa Araujo <larissa.jvsa@gmail.com>
2022-03-08 10:12:25 -0300

    feat: :zap: corrigindo componente da lab PieRechart

--- src/components/PieRechart.js
+++ src/components/PieRechart.js
@@ -31,22 +31,6 @@ function PieRechart() {
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
           </Pie>
-          {/* <Pie
-            data={data}
-            cx={420}
-            cy={200}
-            startAngle={180}
-            endAngle={0}
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-            paddingAngle={5}
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie> */}
           <Tooltip />
         </PieChart>
       </ResponsiveContainer>
