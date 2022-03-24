f3c6c47526a04c516f344b92dfaf78a1e8775356
Fernando Serpa <serpaef@outlook.com>
2022-03-24 09:29:37 -0300

    Revert "refact: CSS login page"
    This reverts commit db993310770c35e0c6acf3569676486218ee4ec4.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from '../image/logo.png';
-import '../styles/Login.css';
-import loginPost from '../services/Login';
+import React from "react";
+import logo from "../image/logo.png";
+import "../styles/Login.css";
+import loginPost from "../services/Login";
 
 class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     };
   }
 
@@ -17,11 +17,11 @@ class Login extends React.Component {
     const { email, password } = this.state;
     const result = await loginPost(email, password);
     if (result.status !== 200) {
-      alert('deu ruim');
+      alert("deu ruim");
     } else {
-      localStorage.setItem('token', result.data);
+      localStorage.setItem("token", result.data);
       const { history } = this.props;
-      history.push('/form');
+      history.push("/form");
     }
   };
 
@@ -31,50 +31,57 @@ class Login extends React.Component {
       [name]: value,
     });
   };
-
+  /*  validEmail = (email) => {
+            const emailRegex = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
+        return emailRegex.test(email)
+    } */
   render() {
     return (
-      <div className='login-bg'>
-        <section className='login'>
-          <article>
-            <img src={logo} alt='logo imagem' className='image-login' />
-          </article>
+      
+        
+      
+      <section className="login">
+        <article>
+          <img src={logo} alt="logo imagem" className="image-login" />
+        </article>
 
-          <form className='label-email'>
-            <label htmlFor='email' className='email'>
-              <input
-                type='text'
-                placeholder='User Name'
-                name='email'
-                id='email'
-                autoComplete='off'
-                onChange={this.takeInfoUser}
-                className='input-email'
-              />
-            </label>
+        <form className="label-email">
 
-            <label htmlFor='input-password' className='password'>
-              <input
-                type='password'
-                placeholder='Enter your password'
-                className='input-password'
-                name='password'
-                autoComplete='off'
-                onChange={this.takeInfoUser}
-                id='password'
-              />
-            </label>
+          <label htmlFor="email" className="email">
+            <input
+              type="text"
+              placeholder="User Name"
+              name="email"
+              id="email"
+              autoComplete="off"
+              onChange={this.takeInfoUser}
+              className="input-email"
+            />
+          </label>
 
-            <button
-              type='submit'
-              className='button-login'
-              onClick={this.handleClick}
-            >
-              Entrar
-            </button>
-          </form>
-        </section>
-      </div>
+          <label htmlFor="input-password" className="password">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="input-password"
+              name="password"
+              autoComplete="off"
+              onChange={this.takeInfoUser}
+              id="password"
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="button-login"
+            onClick={this.handleClick}
+          >
+            Entrar
+          </button>
+        </form>
+      </section>
+     
+      
     );
   }
 }
